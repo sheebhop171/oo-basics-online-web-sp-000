c4c6007a67c34527a9b54aca925008e3c0f4530d
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-13 23:17:31 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,6 @@
 class Book
+
+  attr_accessor :author, :page_count, :genre
   def initialize(title)
       @title = title
   end
@@ -7,31 +9,7 @@ class Book
     @title
   end
 
-  def author=(author) #setter
-    @author = author
-  end
-
-  def author #getter
-    @author
-  end
-
-  def page_count=(num)
-    @page_count = num
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
-
   def turn_page
-    puts "Flipping the page...wow, you read fast!" 
+    puts "Flipping the page...wow, you read fast!"
   end
 end
