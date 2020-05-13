611a1d3881ef362e8eff452ffb20531522bf266e
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-13 23:17:50 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,9 @@
 class Book
 
   attr_accessor :author, :page_count, :genre
-  
+  attr_reader :title
   def initialize(title)
       @title = title
-  end
-
-  def title #getter
-    @title
   end
 
   def turn_page
